aff40b42eb8926dfe92dfb7a0e90e835ce0cfee1
github username <github email address>
2018-01-16 20:47:54 -0500

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,10 +1,6 @@
-
 class RecipesController < ApplicationController
   def show
     @recipe = Recipe.find(params[:id])
-    if @recipe.ingredients.last.try(:name)
-      @recipe.ingredients.build
-    end
   end
 
   def index
@@ -17,19 +13,13 @@ class RecipesController < ApplicationController
   end
 
   def create
-    recipe = Recipe.create(recipe_params)
-    redirect_to recipe
-  end
-
-  def update
-    recipe = Recipe.find(params[:id])
-    recipe.update(recipe_params)
-    redirect_to recipe
+    @recipe = Recipe.create(recipe_params)
+    redirect_to @recipe
   end
 
   private
 
   def recipe_params
-    params.require(:recipe).permit(:title, ingredients_attributes: [:id, :name, :quantity])
+    params.require(:recipe).permit(:title, ingredients_attributes: [:name, :quantity])
   end
 end
